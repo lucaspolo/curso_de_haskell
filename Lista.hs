25383c7001c99df04a59d576682bd97f4c98e57a
Lucas Polo <pradopolo@gmail.com>
2018-08-27 13:52:03 -0300

    Criada função comp_listas
    comp_listas: verifica se duas listas são iguais
    
    close #8

--- Lista.hs
+++ Lista.hs
@@ -1,7 +1,20 @@
 -- |Função size que retorna o tamanho da lista
-
 -- |Quando vazia, tamanho zero
 size [] = 0
 
 -- |x é a cabeça da lista (de tamanho um), xs é o restante da lista
 size (x:xs) = 1 + size xs
+
+-- |Função que verifica se duas listas são iguais (# elementos, na mesma posição)
+
+comp_listas :: [Int] -> [Int] -> Bool
+
+-- |Caso ambas vazias, são iguais
+comp_listas [] [] = True
+-- |Caso uma delas vazia e a outra não, são diferentes
+comp_listas [] _ = False
+comp_listas _ [] = False
+
+-- |Se o primeiro elemento igual, compara outros elementos chamando recursivamente
+comp_listas (x:xs) (y:ys) | (x == y) = comp_listas xs ys
+                          | otherwise = False
--- README.md
+++ README.md
@@ -150,3 +150,19 @@ size [] = 0
 -- |x é a cabeça da lista (de tamanho um), xs é o restante da lista
 size (x:xs) = 1 + size xs
 ```
+
+Para compararmos duas listas, podemos definir uma função que verifica recursivamente as listas:
+
+```haskell
+comp_listas :: [Int] -> [Int] -> Bool
+
+-- |Caso ambas vazias, são iguais
+comp_listas [] [] = True
+-- |Caso uma delas vazia e a outra não, são diferentes
+comp_listas [] _ = False
+comp_listas _ [] = False
+
+-- |Se o primeiro elemento igual, compara outros elementos chamando recursivamente
+comp_listas (x:xs) (y:ys) | (x == y) = comp_listas xs ys
+                          | otherwise = False
+```
